dcc95b2f9de1d40f992f2be221a391b745d294c9
sudhir nimavat <sudhir@nimavat.me>
2018-01-26 15:23:13 +0530

    first commit

---
+++ src/main/groovy/org/codenarc/rule/groovy/CompileStaticRule.groovy
@@ -0,0 +1,43 @@
+package org.codenarc.rule.groovy
+
+import groovy.transform.CompileStatic
+import org.codehaus.groovy.ast.AnnotatedNode
+import org.codehaus.groovy.ast.AnnotationNode
+import org.codehaus.groovy.ast.ClassNode
+import org.codenarc.rule.AbstractAstVisitor
+import org.codenarc.rule.AbstractAstVisitorRule
+
+@CompileStatic
+class CompileStaticRule extends AbstractAstVisitorRule {
+    int priority = 2
+    String name = 'CompileStatic'
+    Class astVisitorClass = GrailsTransactionalVisitor
+}
+
+class GrailsTransactionalVisitor extends AbstractAstVisitor {
+    private static final String GRAILS_COMPILE_STATIC = 'GrailsCompileStatic'
+    private static final String COMPILE_STATIC = "CompileStatic"
+    private static final String COMPILE_DYNAMIC = "CompileDynamic"
+
+    private static final String ERROR_MSG = 'Class should be marked with one of @GrailsCompileStatic, @CompileStatic or @CompileDynamic'
+
+
+    @Override
+    void visitClassEx(ClassNode classNode) {
+        boolean isExplicitlyMarked = false
+
+        for(AnnotationNode annotationNode: classNode.annotations) {
+            String annotation = annotationNode.classNode.text
+            if (annotation in [GRAILS_COMPILE_STATIC, COMPILE_STATIC, COMPILE_DYNAMIC]) {
+               isExplicitlyMarked = true
+            }
+        }
+
+        if(!isExplicitlyMarked) {
+            addViolation(classNode, ERROR_MSG)
+        }
+
+        super.visitClassEx(classNode)
+    }
+
+}
---
+++ src/test/groovy/org/codenarc/rule/groovy/CompileStaticRuleTest.groovy
@@ -0,0 +1,72 @@
+package org.codenarc.rule.groovy
+
+import org.codenarc.rule.AbstractRuleTestCase
+import org.codenarc.rule.Rule
+import org.codenarc.rule.Violation
+import org.junit.Test
+
+class CompileStaticRuleTest extends AbstractRuleTestCase {
+    boolean  skipTestThatUnrelatedCodeHasNoViolations = true
+
+    @Override
+    protected Rule createRule() {
+        return new CompileStaticRule()
+    }
+
+    @Test
+    void "test class without explicit compile static not allowed"() {
+        final source = """
+            
+            class Test {
+            
+            }
+         """
+
+        assertSingleViolation(source) { Violation violation ->
+            violation.rule.priority == 2
+            violation.rule.name == 'CompileStatic'
+        }
+    }
+
+    @Test
+    void "test class with GrailsCompileStatic is allowed"() {
+        final source = """
+            import grails.compiler.GrailsCompileStatic
+            
+            @GrailsCompileStatic
+            class Test {
+            
+            }
+         """
+
+        assertNoViolations(source)
+    }
+
+    @Test
+    void "test class with CompileStatic is allowed"() {
+        final source = """
+            import groovy.transform.CompileStatic
+            
+            @CompileStatic
+            class Test {
+            
+            }
+         """
+
+        assertNoViolations(source)
+    }
+
+    @Test
+    void "test class with CompileDynamic is allowed"() {
+        final source = """
+            import groovy.transform.CompileDynamic
+            
+            @CompileDynamic
+            class Test {
+            
+            }
+         """
+
+        assertNoViolations(source)
+    }
+}
