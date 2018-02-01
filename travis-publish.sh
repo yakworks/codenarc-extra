910db2e60a275bead0d8c70b5163524447c89900
sudhir nimavat <sudhir@nimavat.me>
2018-02-01 17:37:23 +0530

    Add travis

---
+++ travis-publish.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_REPO_SLUG == "yakworks/codenarc-extra" && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+
+    if [[ -n $TRAVIS_TAG ]]
+    then
+        echo "### publishing release to BinTray"
+        ./gradlew gorm-tools:bintrayUpload --no-daemon
+    fi
+
+else
+  echo "Not a Tag, not publishing"
+  echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
+  echo "TRAVIS_TAG: $TRAVIS_TAG"
+  echo "TRAVIS_REPO_SLUG: $TRAVIS_REPO_SLUG"
+  echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
+fi
